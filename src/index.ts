d0c406c3787ca277aa4d9f41ef517d194a25b5b0
lockedNLevered <shaynel@sfu.ca>
2021-12-13 11:01:59 -0800

    User already in Queue will not be requeued, switch to async await from promise

--- src/index.ts
+++ src/index.ts
@@ -13,35 +13,34 @@ const redis = new Redis();
 
 /* Add functionality here */
 app.message("add", async ({ message, say }: any) => {
+	const r = await redis.exists("myset", message.user);
+	if (r) {
+		const rankRes = await redis.zrank("myset", message.user);
+		say(`You are already in the Queue in spot ${rankRes + 1}.`);
+		return;
+	}
+
 	redis.zadd("myset", Date.now(), message.user);
 	await say(`Hello, <@${message.user}>`);
-	redis
-		.zrevrange("myset", 0, -1)
-		.then((res) =>
-			redis
-				.zrank("myset", message.user)
-				.then((res) =>
-					say(
-						`Added <@${message.user}> to Q. You are in spot ${res + 1} :tada:`
-					)
-				)
-		);
+	const rankRes = await redis.zrank("myset", message.user);
+	await say(
+		`Added <@${message.user}> to Q. You are in spot ${rankRes + 1} :tada:`
+	);
 });
 app.message("where", async ({ message, say }: any) => {
-	redis.zrank("myset", message.user).then((res) => {
-		res++;
-		if (res) {
-			say(`You're ${res} spots away`);
-		} else {
-			say("You're not in the Q");
-		}
-	});
+	const res = await redis.zrank("myset", message.user);
+	//Queue is zero indexed so add one
+	const userRank = res + 1;
+	if (userRank) {
+		say(`You're ${userRank} spots away`);
+	} else {
+		say("You're not in the Q");
+	}
 });
 
 app.message("remove", async ({ message, say }: any) => {
-	redis
-		.zrem("myset", message.user)
-		.then(() => say(`Removed <@${message.user}> from queue`));
+	await redis.zrem("myset", message.user);
+	await say(`Removed <@${message.user}> from queue`);
 });
 (async () => {
 	// Start the app
